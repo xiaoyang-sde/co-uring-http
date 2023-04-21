25a01fba543e077e51cd06874e00c0cf96d8b728
Xiaoyang Liu <siujoeng.lau@gmail.com>
2023-04-21 17:24:51 -0400

    feat: implement 'co_uring_http::thread_pool'

---
+++ include/thread_pool.hpp
@@ -0,0 +1,45 @@
+#ifndef THREADPOOL_HPP
+#define THREADPOOL_HPP
+
+#include <condition_variable>
+#include <coroutine>
+#include <list>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+namespace co_uring_http {
+class thread_pool {
+public:
+  explicit thread_pool(const std::size_t thread_count) noexcept;
+  ~thread_pool() noexcept;
+
+  class schedule_awaiter {
+  public:
+    schedule_awaiter(thread_pool &thread_pool);
+
+    constexpr auto await_ready() const noexcept -> bool { return false; };
+    constexpr auto await_resume() const noexcept -> void { return; };
+    auto await_suspend(std::coroutine_handle<> handle) const noexcept -> void;
+
+  private:
+    thread_pool &thread_pool;
+  };
+
+  auto schedule() -> schedule_awaiter;
+
+private:
+  std::mutex mutex;
+  std::condition_variable condition_variable;
+
+  std::stop_source stop_source;
+  std::list<std::jthread> thread_list;
+  std::queue<std::coroutine_handle<>> coroutine_queue;
+
+  auto thread_loop() -> void;
+
+  auto enqueue(std::coroutine_handle<> handle) -> void;
+};
+} // namespace co_uring_http
+
+#endif
---
+++ src/thread_pool.cpp
@@ -0,0 +1,49 @@
+#include "thread_pool.hpp"
+
+co_uring_http::thread_pool::thread_pool(
+    const std::size_t thread_count) noexcept {
+  for (size_t _ = 0; _ < thread_count; ++_) {
+    thread_list.emplace_back([this]() { this->thread_loop(); });
+  }
+};
+
+co_uring_http::thread_pool::~thread_pool() noexcept {
+  stop_source.request_stop();
+  condition_variable.notify_all();
+};
+
+co_uring_http::thread_pool::schedule_awaiter::schedule_awaiter(
+    class thread_pool &thread_pool)
+    : thread_pool{thread_pool} {}
+
+auto co_uring_http::thread_pool::schedule_awaiter::await_suspend(
+    std::coroutine_handle<> handle) const noexcept -> void {
+  thread_pool.enqueue(handle);
+};
+
+auto co_uring_http::thread_pool::schedule() -> schedule_awaiter {
+  return schedule_awaiter{*this};
+}
+
+auto co_uring_http::thread_pool::thread_loop() -> void {
+  while (!stop_source.stop_requested()) {
+    std::unique_lock lock(mutex);
+    condition_variable.wait(lock, [this]() {
+      return stop_source.stop_requested() || !coroutine_queue.empty();
+    });
+    if (stop_source.stop_requested()) {
+      break;
+    }
+
+    const std::coroutine_handle<> coroutine_handle = coroutine_queue.front();
+    coroutine_queue.pop();
+    coroutine_handle.resume();
+  }
+}
+
+auto co_uring_http::thread_pool::enqueue(std::coroutine_handle<> handle)
+    -> void {
+  std::unique_lock lock(mutex);
+  coroutine_queue.emplace(handle);
+  condition_variable.notify_one();
+}
