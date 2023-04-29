1f213aa5694eea81f3accb2d34f21c2d35975002
Xiaoyang Liu <siujoeng.lau@gmail.com>
2023-04-29 13:00:35 -0700

    fix: move the function definitions from 'include/buffer_ring.hpp' to 'src/buffer_ring.cpp'

--- include/buffer_ring.hpp
+++ include/buffer_ring.hpp
@@ -5,7 +5,6 @@
 
 #include <bitset>
 #include <memory>
-#include <optional>
 #include <span>
 
 #include "constant.hpp"
@@ -14,39 +13,11 @@
 namespace co_uring_http {
 class buffer_ring {
 public:
-  static auto get_instance() noexcept -> buffer_ring & {
-    thread_local buffer_ring instance;
-    return instance;
-  }
+  static auto get_instance() noexcept -> buffer_ring &;
 
-  auto register_buffer_ring(const unsigned int buffer_ring_size, const size_t buffer_size) -> void {
-    const size_t ring_entries_size = buffer_ring_size * sizeof(io_uring_buf);
-    const size_t page_alignment = sysconf(_SC_PAGESIZE);
-    void *buffer_ring = nullptr;
-    posix_memalign(&buffer_ring, page_alignment, ring_entries_size);
-    buffer_ring_.reset(reinterpret_cast<io_uring_buf_ring *>(buffer_ring));
-
-    buffer_list_.reserve(buffer_ring_size);
-    for (unsigned int i = 0; i < buffer_ring_size; ++i) {
-      buffer_list_.emplace_back(buffer_size);
-    }
-
-    io_uring_handler::get_instance().setup_buffer_ring(
-        buffer_ring_.get(), buffer_list_, buffer_list_.size()
-    );
-  }
-
-  auto borrow_buffer(const unsigned int buffer_id, const size_t size) -> std::span<std::byte> {
-    borrowed_buffer_set_[buffer_id] = true;
-    return {buffer_list_[buffer_id].data(), size};
-  }
-
-  auto return_buffer(const unsigned int buffer_id) -> void {
-    borrowed_buffer_set_[buffer_id] = false;
-    io_uring_handler::get_instance().add_buffer(
-        buffer_ring_.get(), buffer_list_[buffer_id], buffer_id, buffer_list_.size()
-    );
-  }
+  auto register_buffer_ring(const unsigned int buffer_ring_size, const size_t buffer_size) -> void;
+  auto borrow_buffer(const unsigned int buffer_id, const size_t size) -> std::span<std::byte>;
+  auto return_buffer(const unsigned int buffer_id) -> void;
 
 private:
   std::unique_ptr<io_uring_buf_ring> buffer_ring_;
---
+++ src/buffer_ring.cpp
@@ -0,0 +1,37 @@
+#include "buffer_ring.hpp"
+
+namespace co_uring_http {
+  auto buffer_ring::get_instance() noexcept -> buffer_ring & {
+    thread_local buffer_ring instance;
+    return instance;
+  }
+
+  auto buffer_ring::register_buffer_ring(const unsigned int buffer_ring_size, const size_t buffer_size) -> void {
+    const size_t ring_entries_size = buffer_ring_size * sizeof(io_uring_buf);
+    const size_t page_alignment = sysconf(_SC_PAGESIZE);
+    void *buffer_ring = nullptr;
+    posix_memalign(&buffer_ring, page_alignment, ring_entries_size);
+    buffer_ring_.reset(reinterpret_cast<io_uring_buf_ring *>(buffer_ring));
+
+    buffer_list_.reserve(buffer_ring_size);
+    for (unsigned int i = 0; i < buffer_ring_size; ++i) {
+      buffer_list_.emplace_back(buffer_size);
+    }
+
+    io_uring_handler::get_instance().setup_buffer_ring(
+        buffer_ring_.get(), buffer_list_, buffer_list_.size()
+    );
+  }
+
+  auto buffer_ring::borrow_buffer(const unsigned int buffer_id, const size_t size) -> std::span<std::byte> {
+    borrowed_buffer_set_[buffer_id] = true;
+    return {buffer_list_[buffer_id].data(), size};
+  }
+
+  auto buffer_ring::return_buffer(const unsigned int buffer_id) -> void {
+    borrowed_buffer_set_[buffer_id] = false;
+    io_uring_handler::get_instance().add_buffer(
+        buffer_ring_.get(), buffer_list_[buffer_id], buffer_id, buffer_list_.size()
+    );
+  }
+} // namespace co_uring_http
